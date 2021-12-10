5d58b26a7675204b0d09ffbccee899255874d6c6
atom996 <alpin.tomorri@outlook.com>
2021-12-10 14:04:09 -0500

    First files added to the repository. Mainly to get db.json working.

---
+++ app_dev.js
@@ -0,0 +1,13 @@
+//Express instance
+var express = require('express');
+var app = express();
+
+//Routes
+
+app.get('/api/byName/:nameInput', () => {
+  
+});
+
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!');
+});
