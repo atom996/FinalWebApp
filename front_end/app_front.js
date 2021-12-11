97b8849182eba788cf1b6a5dad8872c197d21a78
atom996 <alpin.tomorri@outlook.com>
2021-12-10 19:41:39 -0500

    Fixed links for app_front.js

--- front_end/app_front.js
+++ front_end/app_front.js
@@ -5,19 +5,19 @@ currently in the text box after button is pressed.*/
 var updateView = async (button) => {
   if(document.getElementById('dropSelect').value == 'opOne') {
     let queryValue = document.querySelector("#usrSpecific").value;
-    apiLink = `http://localhost:3000/api/byCourse/${queryValue}`;
+    apiLink = `https://atom996webapp.herokuapp.com/api/byCourse/${queryValue}`;
   } else if (document.getElementById('dropSelect').value == 'opTwo') {
     let queryValue = document.querySelector("#usrSpecific").value;
-    apiLink = `http://localhost:3000/api/byCode/${queryValue}`;
+    apiLink = `https://atom996webapp.herokuapp.com/api/byCode/${queryValue}`;
   } else if (document.getElementById('dropSelect').value == 'opThree') {
     let queryValue = document.querySelector("#usrSpecific").value;
-    apiLink = `http://localhost:3000/api/byName/${queryValue}`;
+    apiLink = `https://atom996webapp.herokuapp.com/api/byName/${queryValue}`;
   } else if (document.getElementById('dropSelect').value == 'opFour') {
     let queryValue = document.querySelector("#usrSpecific").value;
-    apiLink = `http://localhost:3000/api/byLevel/${queryValue}`;
+    apiLink = `https://atom996webapp.herokuapp.com/api/byLevel/${queryValue}`;
   } else {
     let queryValue = document.querySelector("#usrSpecific").value;
-    apiLink = `http://localhost:3000/api/`;
+    apiLink = `https://atom996webapp.herokuapp.com/api/`;
   }
 
   const data = await fetch(apiLink);
